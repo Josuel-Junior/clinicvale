3f6cdec472fa71d251f84294bf5990c8e4de44d6
Josuel Junior <108230374+Josuel-Junior@users.noreply.github.com>
2024-11-18 20:08:32 -0300

    corrected phone number

--- src/shared/components/layout/carouselInstaFeed/CarouselInstaFeed.tsx
+++ src/shared/components/layout/carouselInstaFeed/CarouselInstaFeed.tsx
@@ -3,118 +3,130 @@ import { useEffect, useState } from "react";
 import { Api } from "../../../services/api/Api";
 import { Box, Container, Link, Typography } from "@mui/material";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
 
-import 'swiper/css';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/pagination";
 import { SkeletonCoponent } from "../../skeleton/Skeleton";
 
-
-
 interface ApiResponse {
-    id: string;
-    media_type: string;
-    media_url: string;
-    permalink: string;
+  id: string;
+  media_type: string;
+  media_url: string;
+  permalink: string;
 }
-
 
 export const CarouselInstaFeed: React.FC = () => {
+  const [dataInstaFeed, setDataInstaFeed] = useState<ApiResponse[]>();
 
-    const [dataInstaFeed, setDataInstaFeed] = useState<ApiResponse[]>()
+  const [loading, setLoading] = useState<boolean>(true);
 
-    const [loading, setLoading] = useState<boolean>(true)
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = import.meta.env.VITE_TOKEN_INSTA;
+      const fields = "media_url,media_type,permalink";
+      try {
+        const response: AxiosResponse = await Api.get("access_token=", {
+          params: {
+            access_token: token,
+            fields: fields,
+          },
+        });
 
+        const { data } = response.data;
+        const images = data.filter(
+          (dataApi: ApiResponse) => dataApi.media_type === "IMAGE"
+        );
+        setDataInstaFeed(images.slice(0, 9));
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const token = import.meta.env.VITE_TOKEN_INSTA
-            const fields = "media_url,media_type,permalink";
-            try {
-                const response: AxiosResponse = await Api.get("access_token=", {
-                    params: {
-                        access_token: token,
-                        fields: fields,
-                    },
-                });
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
+    fetchData();
+  }, []);
 
-                const { data } = response.data
-                const images = data.filter((dataApi: ApiResponse) => dataApi.media_type === "IMAGE")
-                setDataInstaFeed(images.slice(0, 9))
-
-                setLoading(false)
-
-            }
-            catch (error) {
-                console.error(error)
-            }
-        }
-
-        fetchData()
-    }, [])
-
-    if (loading) {
-        return (
-            <SkeletonCoponent heightSkeleton={230} widthSkeleton={220} numberOfSkeleton={4} />
-        )
-    }
-
+  if (loading) {
     return (
-        <Box sx={{ width: "100vw" }} maxWidth="lg">
-            <Typography component="h2" variant="h3" sx={{
-                fontSize: {
-                    xs: "1.2rem",
-                    sm: "1.4rem",
-                    md: "1.6rem", fontWeight: "bold"
+      <SkeletonCoponent
+        heightSkeleton={230}
+        widthSkeleton={220}
+        numberOfSkeleton={4}
+      />
+    );
+  }
 
-                }, textAlign: "center", my: "25px"
-            }}>
-                CONFIRA NOSSAS PUBLICAÇÕES
-            </Typography>
-            <Container>
-
-                <Swiper
-                    slidesPerView={1}
-                    spaceBetween={10}
-                    autoplay={{
-                        delay: 2500,
-                        disableOnInteraction: false,
-                    }}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    breakpoints={{
-                        640: {
-                            slidesPerView: 2,
-                            spaceBetween: 20,
-                        },
-                        768: {
-                            slidesPerView: 4,
-                            spaceBetween: 40,
-                        },
-                        1024: {
-                            slidesPerView: 4,
-                            spaceBetween: 30,
-                        },
-                    }}
-                    modules={[Pagination, Autoplay]}
-                    className="mySwiper"
+  return (
+    <Box sx={{ width: "100vw" }} maxWidth="lg">
+      <Typography
+        component="h2"
+        variant="h3"
+        sx={{
+          fontSize: {
+            xs: "1.2rem",
+            sm: "1.4rem",
+            md: "1.6rem",
+            fontWeight: "bold",
+          },
+          textAlign: "center",
+          my: "25px",
+        }}
+      >
+        CONFIRA NOSSAS PUBLICAÇÕES
+      </Typography>
+      <Container>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 30,
+            },
+          }}
+          modules={[Pagination, Autoplay]}
+          className="mySwiper"
+        >
+          {dataInstaFeed?.map((element: ApiResponse) => {
+            return (
+              <SwiperSlide key={element.id}>
+                <Link
+                  href={element.permalink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label={element.id}
                 >
-                    {dataInstaFeed?.map((element: ApiResponse) => {
-                        return (
-                            <SwiperSlide key={element.id}>
-                                <Link href={element.permalink} target="_blank" rel="noopener noreferrer" aria-label={element.id}>
-                                    <Box component="img" src={element.media_url} sx={{ width: "100%", height: "100%", borderRadius: "10px" }} alt={element.id} loading="lazy"/>
-                                </Link>
-                            </SwiperSlide>
-                        )
-                    })}
-                </Swiper>
-            </Container>
-        </Box>
-
-
-    )
-}
+                  <Box
+                    component="img"
+                    src={element.media_url}
+                    sx={{ width: "100%", height: "100%", borderRadius: "10px" }}
+                    alt={element.id}
+                    loading="lazy"
+                  />
+                </Link>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </Container>
+    </Box>
+  );
+};
